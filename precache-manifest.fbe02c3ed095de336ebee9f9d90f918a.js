7acf3c110ce9c0e20ca0537c8f446cda4e931c12
bechainsc <yosra.helal@bechainsc.com>
2020-07-30 20:23:50 +0200

    Updates

---
+++ precache-manifest.fbe02c3ed095de336ebee9f9d90f918a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970104134d2ee9498aa6ac4d7af40484",
+    "url": "/whitelist-system/index.html"
+  },
+  {
+    "revision": "fa7bc894cc1a2cea6829",
+    "url": "/whitelist-system/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "e9459eab32e069a618de",
+    "url": "/whitelist-system/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fa7bc894cc1a2cea6829",
+    "url": "/whitelist-system/static/js/2.7811c71d.chunk.js"
+  },
+  {
+    "revision": "e9459eab32e069a618de",
+    "url": "/whitelist-system/static/js/main.4bf2d51b.chunk.js"
+  },
+  {
+    "revision": "4ee9e7c5e3d2ab306438",
+    "url": "/whitelist-system/static/js/runtime-main.6144a6ae.js"
+  }
+]);
